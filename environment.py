f1c6f035d0b73af9540151a4a20918bfb2eef965
madhusivaraj <madhuksivaraj@gmail.com>
2019-10-07 12:13:00 -0400

    check neighbors

--- environment.py
+++ environment.py
@@ -5,6 +5,8 @@ import random
 class Minesweeper:
     dimension=8
     density=0.2
+    map = generate_board()
+
 
     def __init__(self, dims=dimension,
                  mine_density=density):
@@ -14,21 +16,45 @@ class Minesweeper:
         self.mines_hit = np.zeros((self.dims, self.dims), dtype=bool)
         self.flag = np.zeros((self.dims, self.dims), dtype=object)
 
-
-    def generate_board(self):
-        board = np.zeros([self.dims,self.dims],dtype=int)
+    def generate_board(self):  # mines are 0
+        board = np.zeros([self.dims, self.dims], dtype=int)
         for i in range(self.mines):
             while 1:
-                row = random.randint(0, self.dims-1)
-                col = random.randint(0, self.dims-1)
+                row = random.randint(0, self.dims - 1)
+                col = random.randint(0, self.dims - 1)
                 if board[row, col] != 1:
                     board[row, col] = 1
                     break
         return board
 
-    def clue_number(board):
-        return 2
-        def find_mine_neighbors(board, row, cell):
-            return 2
+    board = generate_board(self)
 
-print(Minesweeper.generate_board(Minesweeper()))
+    def check_neighbors(board, row, col):
+        mines = 0
+        if int(row - 1) >= 0:  # check the north neighbor
+            if board[int(row - 1)][int(col)] == 1:
+                mines += 1
+        if int(row + 1) <= dimension - 1:  # check the south neighbor
+            if board[int(row + 1)][int(col)] == 1:
+                mines += 1
+        if int(col - 1) >= 0:  # check the west neighbor
+            if board[int(row)][int(col - 1)] == 1:
+                mines += 1
+        if int(col + 1) <= 6:  # check the east neighbor
+            if board[int(row)][int(col + 1)] == 1:
+                mines += 1
+        if int(row - 1) >= 0 and int(col - 1) >= 0:  # check the northwest neighbor
+            if board[int(row - 1)][int(col - 1)] == 1:
+                mines += 1
+        if int(row - 1) >= 0 and int(col + 1) <= 6:  # check the northeast neighbor
+            if board[int(row - 1)][int(col + 1)] == 1:
+                mines += 1
+        if int(row + 1) <= 7 and int(col - 1) >= 0:  # check the southwest neighbor
+            if board[int(row + 1)][int(col - 1)] == 1:
+                mines += 1
+        if int(row + 1) <= 7 and int(col + 1) <= 6:  # check the southeast neighbor
+            if board[int(row + 1)][int(col + 1)] == 1:
+                mines += 1
+        return mines
+
+    print(check_neighbors(board, 2, 2))
